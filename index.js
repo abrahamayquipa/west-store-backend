d2b9de991d6ff190d22c943c7bab873a02f2b3e1
abrahamayquipa <abrahamayquipa2000@gmail.com>
2024-03-06 11:27:29 -0500

    feat: update the index.js file

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ import productsRouter from './product.js';
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://west-store-web-app-project-2rbx2r5ti-abrahams-projects-a43793ae.vercel.app'
-}));
+app.use(cors());
 
 app.use('/api', productsRouter);
 
